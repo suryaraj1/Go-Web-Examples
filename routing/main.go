2a825464bbb70e81deeae89bce4e9034394be469
Suryaraj Bhaduri <suryaraj.bhaduri@yahoo.com>
2023-10-21 20:26:10 +0530

    Add request handler methods for POST & GET

--- routing/main.go
+++ routing/main.go
@@ -7,6 +7,18 @@ import (
 	"github.com/gorilla/mux"
 )
 
+func CreateBook(w http.ResponseWriter, r *http.Request) {
+	vars := mux.Vars(r)
+	title := vars["title"]
+	fmt.Fprintf(w, "Creating a book: %s\n", title)
+}
+
+func ReadBook(w http.ResponseWriter, r *http.Request) {
+	vars := mux.Vars(r)
+	title := vars["title"]
+	fmt.Fprintf(w, "Reading the book: %s\n", title)
+}
+
 func main() {
 	r := mux.NewRouter()
 
@@ -17,6 +29,9 @@ func main() {
 
 		fmt.Fprintf(w, "You've requested the book: %s on page %s\n", title, page)
 	})
+
+	r.HandleFunc("/books/{title}", CreateBook).Methods("POST")
+	r.HandleFunc("/books/{title}", ReadBook).Methods("GET")
 
 	http.ListenAndServe(":8080", r)
 }
